912f8698b9da88dfdcb9f231424d859f687578f0
Dozi0116 <oi.dozi0116@gmail.com>
2023-07-06 00:25:52 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { ref, computed, } from 'vue'
+
+const scene = ref('title')
+
+//////////
+
+const welcomeDialogVisible = computed(() => {
+  return scene.value === 'title'
+})
+
+const gameSceneVisible = computed(() => {
+  return scene.value === 'game'
+})
+
+//////////
+
+const onClickStart = () => {
+  scene.value = 'game'
+}
+
+</script>
+
+<template>
+  <el-dialog
+    v-model="welcomeDialogVisible"
+    title="welcome"
+    width="30%"
+  >
+    <h1>MOJIGUESSER</h1>
+    <p>拡大された漢字がなんという文字か、スクロールしながら探ろう！</p>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="onClickStart">
+          start!
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+  <div
+    v-if="gameSceneVisible"
+  >
+    <p>this is game scene!</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  color: red;
+
+  .inner {
+    color: blue;
+  }
+}
+</style>
